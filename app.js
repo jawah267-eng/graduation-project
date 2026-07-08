22d0a90117d3b971e8490233a42bcc5026c35301
jawa <jawah267@gmail.com>
2026-07-09 01:14:25 +0300

    Fix error status code

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 // Global error handling middleware
 app.use((err, req, res, next) => {
-  res.status(err.status || 500).json({
+  res.status(err.statusCode || 500).json({
     success: false,
     message: err.message,
   });
