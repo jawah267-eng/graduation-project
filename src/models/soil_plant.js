c69c4ca5b8ce6f2e427c8fa49ea4468109c70aa0
jawa <jawah267@gmail.com>
2026-07-15 03:27:03 +0300

    update soil

--- src/models/soil_plant.js
+++ src/models/soil_plant.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 
 const soilplant = new mongoose.Schema({
   soil_id: { type: mongoose.Schema.Types.ObjectId, ref: "soil" },
-  plant_id: { type: mongoose.Schema.Types.ObjectId, ref: "plants" },
+  plant_id: { type: mongoose.Schema.Types.ObjectId, ref: "plant" },
 });
+module.exports = mongoose.model("SoilPlant", soilplant);
