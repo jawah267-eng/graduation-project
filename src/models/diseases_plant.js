16d1edff53da75e58f9d2585aebbc01a84bad5d7
jawa <jawah267@gmail.com>
2026-07-30 18:12:33 +0300

    updat6 diseases

--- src/models/diseases_plant.js
+++ src/models/diseases_plant.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const diseas_plant = mongoose.Schema({
   plant_id: { type: mongoose.Schema.Types.ObjectId, ref: "plant" },
   diseases_id: { type: mongoose.Schema.Types.ObjectId, ref: "diseases" },
-  susceptibility: { type: Number }, //درجة الحساسية
+  susceptibility: {
+    type: String,
+    enum: ["very_low", "low", "moderate", "high", "very_high"],
+  }, //درجة الحساسية
 });
 let dis_pl = mongoose.model("diseas_plant", diseas_plant);
 module.exports = dis_pl;
