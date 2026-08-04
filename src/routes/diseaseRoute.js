40cc3dc410409f6148eb989559d9aec587b5b5df
jawa <jawah267@gmail.com>
2026-08-05 00:13:58 +0300

    update image of disease

--- src/routes/diseaseRoute.js
+++ src/routes/diseaseRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const upload = require("../middlewares/uploadMiddleware");
 const controller = require("../controllers/diseaseController");
 
 // جلب الأمراض حسب نبتة المستخدم
