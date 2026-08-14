bd79515f933e9895aee3317d8d7e7c33121ca57a
jawa <jawah267@gmail.com>
2026-08-14 22:09:22 +0300

    add auth7

--- src/routes/fertilizingRoutes.js
+++ src/routes/fertilizingRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const Authservice = require("../services/authService");
 const controller = require("../controllers/fertilizingController");
 
-router.get("/", controller.getFertilizingSchedule);
+router.get("/", Authservice.protect, controller.getFertilizingSchedule);
 
-router.patch("/:userPlantId", controller.markAsFertilized);
+router.patch("/:userPlantId", Authservice.protect, controller.markAsFertilized);
 
 module.exports = router;
--- src/routes/waterRoutes.js
+++ src/routes/waterRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/waterController");
+const Authservice = require("../services/authService");
+
 //GET /api/v1/water
-router.get("/", controller.getWaterSchedule);
+router.get("/", Authservice.protect, controller.getWaterSchedule);
 // PATCH لأننا عم "نحدث" سجل موجود، مش ننشئ جديد
 // الـ userPlantId جاي من الـ URL params
-router.patch("/:userPlantId", controller.markAsWatered);
+router.patch("/:userPlantId", Authservice.protect, controller.markAsWatered);
 
 module.exports = router;
