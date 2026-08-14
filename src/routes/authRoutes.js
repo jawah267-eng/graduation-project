c583e9fb62d19c529dc7840eb9f0320b0d07b779
jawa <jawah267@gmail.com>
2026-08-14 09:54:27 +0300

    add authentication15

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const { SignUpValidator } = require("../utils/validators/authValidator");
+// const express = require("express");
+// const router = express.Router();
+// const { SignUpValidator } = require("../utils/validators/authValidator");
 
-const { SingUp } = require("../services/authService");
+// const { SingUp } = require("../services/authService");
 
 // // هاد مشان تغير كلمة السر
 // router.put(
@@ -11,12 +11,20 @@ const { SingUp } = require("../services/authService");
 //   changeUserPassword,
 // );
 
-router.route("/signup").post(SingUp);
+// router.route("/signup").post(SingUp);
 
 // router
 //   .route("/:id")
 //   .get(getUsertValidator, getUser)
 //   .put(uploadimage, setProfileImage, updateUserValidator, updateUser)
 //   .delete(deleteUserValidator, deleteUser);
+
+// module.exports = router;
+const express = require("express");
+const router = express.Router();
+
+const { SingUp } = require("../services/authService");
+
+router.post("/signup", SingUp);
 
 module.exports = router;
