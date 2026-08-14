8aa337368e542b5dae55e0d0fded63e177ec3e2a
jawa <jawah267@gmail.com>
2026-08-14 20:37:16 +0300

    add autherazation8

--- src/services/authService.js
+++ src/services/authService.js
@@ -97,3 +97,14 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
   next();
 });
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ApiError("You do not have permission to perform this action", 403),
+      );
+    }
+
+    next();
+  };
+};
