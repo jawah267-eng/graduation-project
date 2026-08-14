878e8ed9d9ed564fb5a162d3b9d79141017db23a
jawa <jawah267@gmail.com>
2026-08-14 21:46:47 +0300

    add auth2

--- src/routes/diseaseRoute.js
+++ src/routes/diseaseRoute.js
@@ -2,23 +2,43 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middlewares/uploadMiddleware");
 const controller = require("../controllers/diseaseController");
+const Authservice = require("../services/authService");
 
 // جلب الأمراض حسب نبتة المستخدم
-router.get("/userplant/:userPlantId", controller.getDiseasesByUserPlant);
+router.get(
+  "/userplant/:userPlantId",
+  Authservice.protect, // لأن هذا خاص بـ "نباتاتي"، وما لازم زائر يشوف أمراض نبات شخص آخر.
+  controller.getDiseasesByUserPlant,
+);
 
 // ارجاع كل الامراض
 //اضافة مرض
 router
   .route("/")
-  .get(controller.getAllDiseases)
-  .post(upload.single("image"), controller.createDisease);
+  .get(Authservice.protect, controller.getAllDiseases)
+
+  .post(
+    Authservice.protect,
+    Authservice.restrictTo("admin"),
+    upload.single("image"),
+    controller.createDisease,
+  );
 //ارجاع مرض معين
 //تعديل مرض
 //حذف مرض
 router
   .route("/:id")
-  .get(controller.getDisease)
-  .put(controller.updateDisease)
-  .delete(controller.deleteDisease);
+  .get(Authservice.protect, controller.getDisease)
+  .put(
+    Authservice.protect,
+    Authservice.restrictTo("admin"),
+    controller.updateDisease,
+  )
+
+  .delete(
+    Authservice.protect,
+    Authservice.restrictTo("admin"),
+    controller.deleteDisease,
+  );
 
 module.exports = router;
