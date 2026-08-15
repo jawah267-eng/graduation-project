1c8a6dadf3e1a3a7297f960d5c42555009bae6bc
jawa <jawah267@gmail.com>
2026-08-15 23:18:13 +0300

    add sendemail3

---
+++ test.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const sendEmail = require("./utils/sendEmail");
+
+sendEmail({
+  email: "jawa@gmail.com",
+  subject: "Test Email",
+  message: "Nodemailer شغال تمام 🎉",
+})
+  .then(() => console.log("Email sent successfully"))
+  .catch((err) => console.error("Email error:", err));
